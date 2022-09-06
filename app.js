e59d0d6493c64a7171cec1b18ee743a90a453102
Rishabh Saklani <74286801+rishabhsaklani08@users.noreply.github.com>
2022-09-06 23:41:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Calculator By Rishabh Saklani
 const display1El = document.querySelector(".display-1");
 const display2El = document.querySelector(".display-2");
 const tempResultEl = document.querySelector(".temp-result");
